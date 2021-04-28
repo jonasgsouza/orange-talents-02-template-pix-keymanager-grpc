765238b4364ad6930cc5c8030180ec76300572cd
jonas-souza-zup <jonas.souza@zup.com.br>
2021-04-28 10:09:08 -0300

    Iniciando testes para registro de chave

---
+++ src/test/kotlin/br/com/zup/edu/pix/registry/RegisterPixKeyEndpointTest.kt
@@ -0,0 +1,12 @@
+package br.com.zup.edu.pix.registry
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RegisterPixKeyEndpointTest {
+
+    @Test
+    fun registerKey() {
+    }
+}
